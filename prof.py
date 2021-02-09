07a4830d8eecd1f4198e7227b36682991e182e1e
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-09 10:04:40 +0530

    Add files via upload

---
+++ prof.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+# Web App Title
+st.markdown('''
+# **Know  Your Data 🎯👀💾**
+
+This is an useful project that can help you plan your Data Science Notebook by provding important insights about your data.
+
+
+''')
+
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown('''Made by MAINAK CHAUDHURI''')
+
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Variables Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Upload your dataset')
+    if st.button('Want to try our Sample dataset ?'):
+        # Example data
+        @st.cache
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['x1', 'x2', 'x3', 'x4', 'x5']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Variable Profiling Report**')
+        st_profile_report(pr)
